41bffb85580174a61174d4d9c009fd2a06d4dfe2
Davut can cüci <davutcancuci93@gmail.com>
2025-12-12 16:16:56 +0300

    quiz starter method added in Quiz.java

--- src/Quiz.java
+++ src/Quiz.java
@@ -13,3 +13,27 @@ public class Quiz {
 public void addQuestion(Question q) {
     questions.add(q);
 }
+public void start() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("BEGIN THE QUIZ");
+
+    for (Question q : questions) {
+        System.out.println("QUESTION: " + q.getText());
+
+        if (q instanceof MultipleChoiceQuestion) {
+            ((MultipleChoiceQuestion) q).displayOptions();
+        }
+
+        System.out.print("ANSWER: ");
+        String answer = scanner.nextLine();
+
+        if (q.checkAnswer(answer)) {
+            System.out.println("TRUE!");
+            totalScore += q.getScore();
+        } else {
+            System.out.println("FALSE!");
+        }
+        System.out.println("---------------------");
+    }
+    System.out.println("TOTAL GRADE: " + totalScore);
+}
