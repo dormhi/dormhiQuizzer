832e4bd65702b1bceb142f0b0b9489ad8464cdb5
Davut can cüci <davutcancuci93@gmail.com>
2025-12-19 00:36:14 +0300

    main class modified for new question databasse in Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,21 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Student currentStudent = new Student("Ahmet Yilmaz", "2023101");
+        Student currentStudent = new Student("Ahmet Yilmaz", "2023101"); //just example. it will be upgraded
 
         Quiz myQuiz = new Quiz();
 
-        ArrayList<String> options1 = new ArrayList<>();
-        options1.add("Java");
-        options1.add("Python");
-        options1.add("C++");
-        options1.add("HTML");
+        QuestionLoader loader = new QuestionLoader();
+        ArrayList<Question> fileQuestions = loader.loadQuestions("questions.csv"); // new question databasse
 
-        MultipleChoiceQuestion q1 = new MultipleChoiceQuestion("Which of the following is an interpreted language?", 10, options1, 2);
+        for (Question q : fileQuestions) {
+            myQuiz.addQuestion(q);
+        }
 
-        TrueFalseQuestion q2 = new TrueFalseQuestion("In Java, 'int' is a primitive data type.", 5, true);
-
-        ArrayList<String> options2 = new ArrayList<>();
-        options2.add("1990");
-        options2.add("1995");
-        options2.add("2000");
-        MultipleChoiceQuestion q3 = new MultipleChoiceQuestion("In which year was Java first released?", 15, options2, 2);
-
-
-        myQuiz.addQuestion(q1);
-        myQuiz.addQuestion(q2);
-        myQuiz.addQuestion(q3);
-
-
-        myQuiz.start(currentStudent);
+        if (fileQuestions.size() > 0) {
+            myQuiz.start(currentStudent);
+        } else {
+            System.out.println("No questions found. Please check questions.csv file.");
+        }
     }
 }
