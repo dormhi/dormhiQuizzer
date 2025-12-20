051bb481c152c320f8471df563cc3e2cdde3b554
Davut can cüci <davutcancuci93@gmail.com>
2025-12-20 18:54:58 +0300

    student class chanced the class for super class

--- src/Student.java
+++ src/Student.java
@@ -1,29 +1,15 @@
-public class Student {
-    private String name;
-    private String studentId;
+public class Student extends User {
     private int examScore;
 
-    // Constructor to create a new student
-    public Student(String name, String studentId) {
-        this.name = name;
-        this.studentId = studentId;
-        this.examScore = 0; // Starts with 0 points
-    }
-
-    // Basic Getter methods
-    public String getName() {
-        return name;
-    }
-
-    public String getStudentId() {
-        return studentId;
+    public Student(String id, String fullName, String username, String password) {
+        super(id, fullName, username, password);
+        this.examScore = 0;
     }
 
     public int getExamScore() {
         return examScore;
     }
 
-    // Method to update score
     public void addToScore(int points) {
         if (points > 0) {
             this.examScore += points;
