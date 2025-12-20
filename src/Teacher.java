ba5d29ad946815f036cf35a8920e9218c60b1930
Davut can cüci <davutcancuci93@gmail.com>
2025-12-20 18:36:01 +0300

    teacher class addded and waiting to add new functions in Teacher.java

---
+++ src/Teacher.java
@@ -0,0 +1,7 @@
+public class Teacher extends User {
+    public Teacher(String id, String fullName, String username, String password) {
+        super(id, fullName, username, password);
+    }
+
+    //ı will add new methods in there later
+}
