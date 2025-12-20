5caca95d1721b3d99f1c8b5ef67c6688518a0dee
Davut can cüci <davutcancuci93@gmail.com>
2025-12-20 18:41:33 +0300

    Admin class addded and waiting to add new functions in Admin.java

---
+++ src/Admin.java
@@ -0,0 +1,7 @@
+public class Admin extends User {
+    public Admin(String id, String fullName, String username, String password) {
+        super(id, fullName, username, password);
+    }
+}
+
+//waiting to add new methods}
