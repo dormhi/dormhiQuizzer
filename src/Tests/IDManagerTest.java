d255eb456370646159920c69b3ecc4f6582dc5cc
Davut can cüci <davutcancuci93@gmail.com>
2025-12-20 20:42:36 +0300

    [TEST] IDManager tested in IDManagerTest.java

---
+++ src/Tests/IDManagerTest.java
@@ -0,0 +1,24 @@
+package Tests;
+
+class IDManagerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isIdAllowed() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRole() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addId() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeId() {
+    }
+}
