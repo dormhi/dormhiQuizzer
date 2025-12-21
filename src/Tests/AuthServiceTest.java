f4046e84bba4452126d636425c9f1895df8b0f1f
Davut can cüci <davutcancuci93@gmail.com>
2025-12-21 23:11:21 +0300

    [TEST]auethenticator Junit test class

---
+++ src/Tests/AuthServiceTest.java
@@ -0,0 +1,21 @@
+package Tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void register() {
+    }
+}
