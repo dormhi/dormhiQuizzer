4b2ea835994051cf702f1a7fd2f677f0a5a8527b
Davut can cüci <davutcancuci93@gmail.com>
2025-12-26 11:22:41 +0300

    [TEST] question function tested

---
+++ src/Tests/QuestionLoaderTest.java
@@ -0,0 +1,17 @@
+package Tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionLoaderTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void loadQuestions() {
+    }
+}
