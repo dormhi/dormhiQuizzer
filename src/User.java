82ee8d03fd9653ea1bd10e6c43b48c4d92d27b3e
Davut can cüci <davutcancuci93@gmail.com>
2025-12-30 14:27:13 +0300

    [FEAT] Implemented ICsvConvertible interface for data serialization

--- src/User.java
+++ src/User.java
@@ -1,4 +1,4 @@
-public abstract class User {   //main abstract
+public abstract class User implements ICsvConvertible {
     private String id;       // ID for everyone (Student Number, Teacher ID, or Admin Code)
     private String fullName;
     private String username;
@@ -26,5 +26,10 @@ public abstract class User {   //main abstract
 
     public boolean checkPassword(String inputPassword) {  //pasword line
         return this.password.equals(inputPassword);
+    }
+    //Interface Metod
+    @Override
+    public String toCsvString() {
+        return id + ";" + getClass().getSimpleName().toUpperCase() + ";" + fullName + ";" + username + ";" + password;
     }
 }
