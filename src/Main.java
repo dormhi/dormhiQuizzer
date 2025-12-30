2d05c3e9afe28434d45a1b39d59c2637971cf0fa
Davut can cüci <davutcancuci93@gmail.com>
2025-12-30 15:44:39 +0300

    [TEST] final JUnit tests

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 //main class just starting menu system class now
 public class Main {
     public static void main(String[] args) {
-        // Uygulamayı başlat
         MenuSystem menu = new MenuSystem();
         menu.start();
     }
---
+++ src/Tests/MultipleChoiceQuestionTest.java
@@ -0,0 +1,29 @@
+package Tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultipleChoiceQuestionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getText() {
+    }
+
+    @Test
+    void getScore() {
+    }
+
+    @Test
+    void displayOptions() {
+    }
+
+    @Test
+    void checkAnswer() {
+    }
+}
