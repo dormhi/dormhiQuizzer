219e3d967aae7d50d84c4d1dd1e715ce19e008bb
Davut can cüci <davutcancuci93@gmail.com>
2025-12-31 02:01:01 +0300

    [TEST]AuthService final test

---
+++ src/AuthServiceTest2.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthServiceTest2 {
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void getAllRegisteredUsers() {
+    }
+
+    @Test
+    void updateUserPassword() {
+    }
+}
